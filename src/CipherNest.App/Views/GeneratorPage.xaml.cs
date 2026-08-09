fd82c877921262f417612600561ccac39cc0a288
Sanskar <sanskarin@outlook.in>
2026-08-09 11:37:53 +0530

    feat(ui): refresh generator defaults when page appears
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/CipherNest.App/Views/GeneratorPage.xaml.cs
+++ src/CipherNest.App/Views/GeneratorPage.xaml.cs
@@ -5,9 +5,17 @@ namespace CipherNest.App.Views;
 
 public partial class GeneratorPage : ContentPage
 {
+    private GeneratorViewModel ViewModel => (GeneratorViewModel)BindingContext;
+
     public GeneratorPage()
     {
         InitializeComponent();
         BindingContext = ServiceProviderHelper.GetRequiredService<GeneratorViewModel>();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await ViewModel.LoadDefaultsAsync();
     }
 }
