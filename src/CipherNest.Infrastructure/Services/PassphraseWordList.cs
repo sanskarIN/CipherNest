e5bf5707fcf25fd7a0a1d25897ec0f6ffc9cb116
Sanskar <sanskarin@outlook.in>
2026-08-09 11:29:55 +0530

    feat(generator): add 256-word reviewed local passphrase list
    Signed-off-by: Sanskar <sanskarin@outlook.in>

---
+++ src/CipherNest.Infrastructure/Services/PassphraseWordList.cs
@@ -0,0 +1,24 @@
+namespace CipherNest.Infrastructure.Services;
+
+public static class PassphraseWordList
+{
+    public static IReadOnlyList<string> Words { get; } = new[]
+    {
+        "amber", "anchor", "apple", "apron", "arch", "arrow", "atlas", "autumn", "bamboo", "beacon", "berry", "birch", "blossom", "blue", "breeze", "bridge",
+        "brook", "canyon", "cedar", "cherry", "circle", "cloud", "clover", "coast", "coral", "creek", "crystal", "dawn", "delta", "dune", "eagle", "earth",
+        "echo", "ember", "falcon", "fern", "field", "flame", "forest", "frost", "galaxy", "garden", "glacier", "grove", "harbor", "hazel", "hill", "horizon",
+        "island", "ivory", "jade", "jungle", "lagoon", "lake", "lantern", "leaf", "lemon", "lilac", "lotus", "maple", "meadow", "meteor", "mist", "moon",
+        "moss", "mountain", "nebula", "ocean", "olive", "orchid", "pebble", "pearl", "pine", "planet", "plum", "prairie", "quartz", "rain", "reef", "ridge",
+        "river", "robin", "sage", "sand", "shell", "shore", "sky", "solar", "sparrow", "spring", "stone", "storm", "summit", "sunset", "tiger", "timber",
+        "trail", "valley", "violet", "wave", "willow", "winter", "zenith", "zephyr", "acorn", "alpine", "apricot", "aqua", "aspen", "aurora", "basil", "bay",
+        "birchwood", "bloom", "brookside", "canyonland", "cascade", "chestnut", "citrus", "comet", "copper", "cosmos", "creekside", "daisy", "drift", "elm", "everest", "feather",
+        "fjord", "flora", "gardenia", "garnet", "glade", "golden", "granite", "harborlight", "heather", "indigo", "iris", "juniper", "kelp", "lagoonlight", "larch", "lava",
+        "lavender", "limestone", "magnolia", "marble", "marine", "mesa", "mint", "morning", "north", "oak", "oasislight", "opal", "orchard", "otter", "palm", "pebblelight",
+        "pinecone", "pond", "poppy", "quartzite", "raven", "redwood", "ripple", "rose", "ruby", "seaglass", "silver", "snow", "spruce", "star", "summitlight", "tide",
+        "topaz", "tulip", "tundra", "umber", "vista", "water", "wheat", "wildflower", "wind", "woodland", "yarrow", "yellow", "zephyrine", "acacia", "alder", "amethyst",
+        "anise", "arctic", "barley", "beach", "beech", "bellflower", "boulder", "bramble", "bronze", "buttercup", "caldera", "canary", "caramel", "cardinal", "cinder", "citrine",
+        "cobalt", "cotton", "cove", "cypress", "daylight", "dew", "diamond", "driftwood", "evergreen", "finch", "firefly", "flax", "fog", "foxglove", "freesia", "geode",
+        "ginkgo", "hazelnut", "honey", "hummingbird", "iceberg", "jasmine", "lagoonblue", "lichen", "lily", "linen", "lupine", "marigold", "mica", "midnight", "mulberry", "nickel",
+        "nightingale", "obsidian", "papaya", "peach", "petal", "pollen", "raincloud", "reed", "rosemary", "saffron", "seafoam", "shale", "skylark", "snowdrop", "sorrel", "sprucecone"
+    };
+}
