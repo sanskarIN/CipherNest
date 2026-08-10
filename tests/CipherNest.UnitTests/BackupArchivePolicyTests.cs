31674b23c5dccd2b10d60b7225d8171cf108cab2
Sanskar <sanskarin@outlook.in>
2026-08-10 18:12:13 +0530

    test(backup): cover archive count and aggregate size bounds
    Signed-off-by: Sanskar <sanskarin@outlook.in>

---
+++ tests/CipherNest.UnitTests/BackupArchivePolicyTests.cs
@@ -0,0 +1,28 @@
+using CipherNest.Infrastructure.Services;
+
+namespace CipherNest.UnitTests;
+
+public sealed class BackupArchivePolicyTests
+{
+    [Fact]
+    public void EntryCount_AcceptsSupportedMaximumAndRejectsOverflow()
+    {
+        BackupArchivePolicy.ValidateEntryCount(0);
+        BackupArchivePolicy.ValidateEntryCount(BackupArchivePolicy.MaximumEntryCount);
+
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.ValidateEntryCount(-1));
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.ValidateEntryCount(BackupArchivePolicy.MaximumEntryCount + 1));
+    }
+
+    [Fact]
+    public void AggregateLength_AcceptsExactLimitAndRejectsOverflowWithoutArithmeticWrap()
+    {
+        var almostFull = BackupArchivePolicy.MaximumArchiveBytes - 1;
+        Assert.Equal(BackupArchivePolicy.MaximumArchiveBytes, BackupArchivePolicy.AddEntryLength(almostFull, 1));
+
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.AddEntryLength(almostFull, 2));
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.AddEntryLength(-1, 1));
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.AddEntryLength(0, -1));
+        Assert.Throws<InvalidDataException>(() => BackupArchivePolicy.AddEntryLength(long.MaxValue, long.MaxValue));
+    }
+}
