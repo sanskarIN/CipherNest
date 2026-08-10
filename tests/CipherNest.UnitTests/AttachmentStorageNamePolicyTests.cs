45632f899e7a4a479f20579152a1cddca3e16716
Sanskar <sanskarin@outlook.in>
2026-08-10 18:56:17 +0530

    test(attachments): cover storage-name identity binding
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- tests/CipherNest.UnitTests/AttachmentStorageNamePolicyTests.cs
+++ tests/CipherNest.UnitTests/AttachmentStorageNamePolicyTests.cs
@@ -10,6 +10,7 @@ public sealed class AttachmentStorageNamePolicyTests
         var id = Guid.Parse("01234567-89ab-cdef-0123-456789abcdef");
         Assert.Equal("0123456789abcdef0123456789abcdef.cna", AttachmentStorageNamePolicy.ValidateOpaqueFileName("0123456789ABCDEF0123456789ABCDEF.CNA"));
         Assert.Equal($"{id:N}.cna", AttachmentStorageNamePolicy.ValidateOpaqueFileName($"{id:N}.cna"));
+        Assert.Equal($"{id:N}.cna", AttachmentStorageNamePolicy.ValidateForAttachment(id, $"{id:N}.cna"));
     }
 
     [Theory]
@@ -18,8 +19,19 @@ public sealed class AttachmentStorageNamePolicyTests
     [InlineData("not-a-guid.cna")]
     [InlineData("0123456789abcdef0123456789abcdef.tmp")]
     [InlineData("0123456789abcdef0123456789abcde.cna")]
+    [InlineData("00000000000000000000000000000000.cna")]
     public void MalformedOpaqueName_IsRejected(string value)
     {
         Assert.Throws<InvalidDataException>(() => AttachmentStorageNamePolicy.ValidateOpaqueFileName(value));
+    }
+
+    [Fact]
+    public void MismatchedOpaqueName_IsRejectedForAttachment()
+    {
+        var attachmentId = Guid.Parse("01234567-89ab-cdef-0123-456789abcdef");
+        var otherId = Guid.Parse("fedcba98-7654-3210-fedc-ba9876543210");
+
+        Assert.Throws<InvalidDataException>(() => AttachmentStorageNamePolicy.ValidateForAttachment(Guid.Empty, $"{attachmentId:N}.cna"));
+        Assert.Throws<InvalidDataException>(() => AttachmentStorageNamePolicy.ValidateForAttachment(attachmentId, $"{otherId:N}.cna"));
     }
 }
