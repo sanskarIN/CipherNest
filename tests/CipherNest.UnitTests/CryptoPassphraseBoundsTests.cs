892cdd1cffe71be8252c2073133fbedac46ef89a
Sanskar <sanskarin@outlook.in>
2026-08-10 19:04:13 +0530

    test(crypto): cover passphrase character resource bounds
    Signed-off-by: Sanskar <sanskarin@outlook.in>

---
+++ tests/CipherNest.UnitTests/CryptoPassphraseBoundsTests.cs
@@ -0,0 +1,24 @@
+using CipherNest.Infrastructure.Crypto;
+
+namespace CipherNest.UnitTests;
+
+public sealed class CryptoPassphraseBoundsTests
+{
+    [Fact]
+    public void WrappedKey_RejectsPassphraseBelowMinimumBeforeKdfWork()
+    {
+        var crypto = new CryptoService();
+        var passphrase = new string('a', CryptoService.MinimumPassphraseCharacters - 1);
+
+        Assert.Throws<ArgumentException>(() => crypto.CreateWrappedKey(passphrase.AsSpan()));
+    }
+
+    [Fact]
+    public void WrappedKey_RejectsPassphraseAboveMaximumBeforeKdfWork()
+    {
+        var crypto = new CryptoService();
+        var passphrase = new string('a', CryptoService.MaximumPassphraseCharacters + 1);
+
+        Assert.Throws<ArgumentException>(() => crypto.CreateWrappedKey(passphrase.AsSpan()));
+    }
+}
